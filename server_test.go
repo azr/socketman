26c9e02595718f7ce0233365769ed813be23407d
Adrien Delorme <adrien.delorme@icloud.com>
2016-04-12 12:30:17 +0200

    make sure timeout really fails :) fast ones might success

--- server_test.go
+++ server_test.go
@@ -56,7 +56,7 @@ func TestListenAndServeTimeout(t *testing.T) {
 
 	var err error
 	test(t, server, echoHandler, client, func(c io.ReadWriter) {
-		time.Sleep(server.Config.IdleTimeout)
+		time.Sleep(server.Config.IdleTimeout + 1)
 		for i := 0; i < len(in) && err == nil; {
 			w := 0
 			w, err = io.WriteString(c, in)
